2e4aeda48a444de8cfb98380a1c32b8411ca6eab
elvenapp <138189767+elvenapp@users.noreply.github.com>
2023-06-30 17:59:47 +0800

    Create encrypt.py

---
+++ encrypt.py
@@ -0,0 +1,53 @@
+import base64
+import os
+from Crypto.Cipher import AES
+
+def pkcs7unpadding(text):
+    length = len(text)
+    unpadding = ord(text[length-1])
+    return text[0:length-unpadding]
+
+def decrypt(key, content):
+
+    key_bytes = bytes(key, encoding='utf-8')
+    iv = key_bytes
+    cipher = AES.new(key_bytes, AES.MODE_CBC, iv)
+    # base64解码
+    encrypt_bytes = base64.b64decode(content)
+    # 解密
+    decrypt_bytes = cipher.decrypt(encrypt_bytes)
+    # 重新编码
+    result = str(decrypt_bytes, encoding='utf-8')
+    # 去除填充内容
+    result = pkcs7unpadding(result)
+    return result
+
+aes_key = '5a04ec902686fb05'
+
+def pkcs7padding(text):
+    bs = AES.block_size  # 16
+    length = len(text)
+    bytes_length = len(bytes(text, encoding='utf-8'))
+    # tips：utf-8编码时，英文占1个byte，而中文占3个byte
+    padding_size = length if(bytes_length == length) else bytes_length
+    padding = bs - padding_size % bs
+    # tips：chr(padding)看与其它语言的约定，有的会使用'\0'
+    padding_text = chr(padding) * padding
+    return text + padding_text
+
+def encrypt(key, content):
+    key_bytes = bytes(key, encoding='utf-8')
+    iv = key_bytes
+    cipher = AES.new(key_bytes, AES.MODE_CBC, iv)
+    # 处理明文
+    content_padding = pkcs7padding(content)
+    # 加密
+    encrypt_bytes = cipher.encrypt(bytes(content_padding, encoding='utf-8'))
+    # 重新编码
+    result = str(base64.b64encode(encrypt_bytes), encoding='utf-8')
+    return result
+  
+with open('config.json','r') as c:
+    config = c.read()
+    with open('encrypt_config', 'w') as f:
+        f.write(encrypt(aes_key,config))
